a28728000f05bece81961343baa9d40f01af35d3
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-11-13 14:59:12 -0800

    added login token builder

---
+++ api/auth/token-builder.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken")
+
+const { JWT_SECRET } = require("../../config")
+
+const buildToken = user => {
+  const payload = {
+    subject: user.user_id,
+    username: user.username,
+    email: user.email
+  }
+  const options = {
+    expiresIn: "5m"
+  }
+  const signedToken = jwt.sign(payload, JWT_SECRET, options)
+  return signedToken
+}
+
+module.exports = buildToken

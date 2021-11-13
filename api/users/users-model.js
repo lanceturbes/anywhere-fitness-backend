cef748cffb0002b6f33b74472a8ba854cf57c44f
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-11-13 14:59:47 -0800

    added users model w/ db helpers

---
+++ api/users/users-model.js
@@ -0,0 +1,39 @@
+const db = require("../../data/db-config")
+
+const getAll = async () => {
+  const users = await db("users")
+  return users
+}
+
+const getById = async (id) => {
+  const user = await db("users")
+    .where({ user_id: id })
+    .first()
+  return user
+}
+
+const filterBy = async (filter) => {
+  const filteredUsers = await db("users")
+    .where(filter)
+  return filteredUsers
+}
+
+const add = async (user) => {
+  const insertResult = await db("users")
+    .insert(user)
+    .returning([
+      "user_id",
+      "username",
+      "password",
+      "email"
+    ])
+  const newUser = insertResult[0]
+  return newUser
+}
+
+module.exports = {
+  getAll,
+  getById,
+  filterBy,
+  add
+}

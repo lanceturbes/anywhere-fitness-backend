a633b1d65f0dd912afc65dccf03d3883df4282b0
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-11-13 15:02:31 -0800

    updated server.js with new routers/middleware

--- api/server.js
+++ api/server.js
@@ -1,10 +1,27 @@
+// Libraries
 const express = require("express")
 const helmet = require("helmet")
 const cors = require("cors")
 
+// Custom Middleware
+const handleError = require("./middleware/handle-error")
+const authRouter = require("./auth/auth-router")
+const usersRouter = require("./users/users-router")
+
+// Server Instantiation
 const server = express()
 
+// Middleware Application
+server.use(express.json())
 server.use(helmet())
 server.use(cors())
 
+// Routers
+server.use("/api/auth", authRouter)
+server.use("/api/users", usersRouter)
+
+// Error Handler
+server.use(handleError)
+
+// Server Export
 module.exports = server

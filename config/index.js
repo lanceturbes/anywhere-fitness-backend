c03b396ce4c12e2db2b02d90cac5753a64920de1
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-11-14 13:05:29 -0800

    ensured certain env vars are parsed correctly

--- config/index.js
+++ config/index.js
@@ -1,11 +1,11 @@
 require("dotenv").config()
 
 module.exports = {
-  PORT: process.env.PORT || 9000,
+  PORT: parseInt(process.env.PORT) || 9000,
   NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL: process.env.DATABASE_URL,
   DATABASE_URL_DEV: process.env.DATABASE_URL_DEV || "no development database url set",
   DATABASE_URL_TESTING: process.env.DATABASE_URL_TESTING || "no testing database url set",
   JWT_SECRET: process.env.JWT_SECRET || "keep it secret; keep it safe!",
-  BCRYPT_ROUNDS: process.env.BCRYPT_ROUNDS || 6
+  BCRYPT_ROUNDS: parseInt(process.env.BCRYPT_ROUNDS) || 6
 }

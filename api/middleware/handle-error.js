e2a91441416302959f921b1c89277afa840f09ff
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-11-14 12:58:34 -0800

    temporarily changed error handler to be verbose

--- api/middleware/handle-error.js
+++ api/middleware/handle-error.js
@@ -1,10 +1,11 @@
-const { NODE_ENV } = require("../../config")
+// const { NODE_ENV } = require("../../config")
 
 const handleError = (err, req, res, next) => {
   res.status(err.status || 500).json({
-    message: NODE_ENV === "production"
-      ? "Oh, d-d-d-dear!"
-      : err.message || "Oh, d-d-d-dear!"
+    // message: NODE_ENV === "production"
+    //   ? "Oh, d-d-d-dear!"
+    //   : err.message || "Oh, d-d-d-dear!"
+    message: err.message
   })
 }
 

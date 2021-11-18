19254f641d328a4c9bdf91bd8eb21b26889bdff3
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-11-18 07:11:13 -0800

    updated classes_clients seed file

--- data/seeds/007-class-clients.js
+++ data/seeds/007-class-clients.js
@@ -1,12 +1,11 @@
 const classes_clients = [
-  {
-    user_id: 3,
-    class_id: 1
-  },
-  {
-    user_id: 2,
-    class_id: 1
-  }
+  { class_id: 1, user_id: 1 },
+  { class_id: 1, user_id: 2 },
+  { class_id: 1, user_id: 3 },
+  { class_id: 2, user_id: 1 },
+  { class_id: 3, user_id: 5 },
+  { class_id: 4, user_id: 4 },
+  { class_id: 5, user_id: 3 }
 ]
 
 exports.seed = function (knex) {
